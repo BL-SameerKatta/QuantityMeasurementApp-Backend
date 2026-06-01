06fdbee78d70879966dae8ca014e5fc33786b0bb
Sameer Katta <sameerkatta18@gmail.com>
2026-06-02 03:21:04 +0530

    feat: Add root health check endpoint to conversion-service

---
+++ conversion-service/src/main/java/com/apps/QuantityMeasurementApp/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.apps.QuantityMeasurementApp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("conversion service working fine");
+    }
+}
